8689c71d3d3e4e03e76f92479eb028421f3ef792
AfrazAther <126904780+AfrazAther@users.noreply.github.com>
2023-06-05 21:36:03 -0600

    fix

---
+++ components/Sidebar.tsx
@@ -0,0 +1,81 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+import { useMemo } from 'react';
+import { HiOutlineSearchCircle } from 'react-icons/hi'
+import { FaHome } from 'react-icons/fa'
+import Box from './Box';
+import SidebarItem from './SidebarItem';
+import Library from './Library';
+
+interface SidebarProps {
+    children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({
+    children
+}) => {
+
+    const pathname = usePathname();
+
+    const routes = useMemo(() => [
+        {
+            icon: FaHome,
+            label: 'Home',
+            active: pathname !== '/search',
+            href: '/',
+        },
+        {
+            icon: HiOutlineSearchCircle,
+            label: 'Search',
+            active: pathname === '/search',
+            href: '/search',
+        }
+    ], [pathname])
+
+  return (
+    <div className='flex h-full'>
+        <div
+            className='
+                hidden
+                md:flex
+                flex-col
+                gap-y-2
+                bg-black
+                h-full
+                w-[300px]
+                p-2
+            '
+        >
+            <Box>
+                <div
+                    className='
+                        flex
+                        flex-col
+                        gap-y-4
+                        px-5
+                        py-4
+                    '
+                >
+                    {routes.map((item) => (
+                        <SidebarItem
+                            key={item.label}
+                            {...item}
+                        />
+                    ) )}
+                </div>
+            </Box>
+            <Box className='overflow-y-auto h-full'>
+                <Library
+                
+                />
+            </Box>
+        </div>
+        <main className='h-full flex-1 oveflow-y-auto py-2'>
+            {children}
+        </main>
+    </div>
+  )
+}
+
+export default Sidebar
