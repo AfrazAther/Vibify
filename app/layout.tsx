d671580056e38e0cd6c8dccdb87f2181a5c22978
Kara Afraz <126904780+AfrazAther@users.noreply.github.com>
2023-06-08 20:44:55 -0600

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
 import { Figtree } from 'next/font/google'
 
 import getSongsByUserId from '@/actions/getSongsByUserId'
-
+import getActiveProductsWithPrices from '@/actions/getActiveProductsWithPrices'
 import Sidebar from '@/components/Sidebar'
 import ToasterProvider from '@/providers/ToasterProvider'
 import UserProvider from '@/providers/UserProvider'
+import ModalProvider from '@/providers/ModalProvider'
 import SupabaseProvider from '@/providers/SupabaseProvider'
-
+import Player from '@/components/Player'
 
 import './globals.css'
-import Player from '@/components/Player'
 
 const font = Figtree({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Vibify',
-  description: 'Just Vibe',
+  title: 'Spotify Clone',
+  description: 'Spotify Clone',
 }
 
 export const revalidate = 0;
@@ -25,7 +25,7 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-
+  const products = await getActiveProductsWithPrices();
   const userSongs = await getSongsByUserId();
 
   return (
@@ -33,11 +33,12 @@ export default async function RootLayout({
       <body className={font.className}>
         <ToasterProvider />
         <SupabaseProvider>
-          <UserProvider>            
+          <UserProvider>
+            <ModalProvider products={products} />
             <Sidebar songs={userSongs}>
               {children}
             </Sidebar>
-            <Player/>          
+            <Player />
           </UserProvider>
         </SupabaseProvider>
       </body>
